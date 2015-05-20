1d3283c055d6d186c3a5dc8bb9408eeedbead152
Guido Zockoll <gzockoll@gmail.com>
2015-05-20 20:40:45 +0200

    increase upload limit

--- src/main/java/de/gzockoll/prototype/assets/AssetRepositoryApplication.java
+++ src/main/java/de/gzockoll/prototype/assets/AssetRepositoryApplication.java
@@ -2,9 +2,12 @@ package de.gzockoll.prototype.assets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class AssetRepositoryApplication {
@@ -21,5 +24,13 @@ public class AssetRepositoryApplication {
     @Bean
     public LocalValidatorFactoryBean validator() {
         return new LocalValidatorFactoryBean();
+    }
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("5120MB");
+        factory.setMaxRequestSize("5120MB");
+        return factory.createMultipartConfig();
     }
 }
