6ade42b16b8177ba6e722c5ec2c8c9eee10830bd
Guido Zockoll <gzockoll@gmail.com>
2015-08-08 21:27:50 +0200

    tests fixed

--- src/test/java/de/gzockoll/prototype/assets/boundary/AssertResourceTest.java
+++ src/test/java/de/gzockoll/prototype/assets/boundary/AssertResourceTest.java
@@ -5,6 +5,7 @@ import com.mongodb.gridfs.GridFSDBFile;
 import de.gzockoll.prototype.assets.AssetRepositoryApplication;
 import de.gzockoll.prototype.assets.entity.Asset;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,8 +15,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,33 +35,50 @@ public class AssertResourceTest {
     @Test
     public void testSave() throws Exception {
         String data="bla";
-        Asset asset=new Asset(new File("bla.txt"));
-        resource.save(asset);
+        final File input = File.createTempFile("bla",".tmp");
+        PrintWriter pw = new PrintWriter(input);
+        pw.write(data);
+        pw.close();
+        try {
+            Asset asset=new Asset(input);
+            resource.save(asset);
 
 
-        List<GridFSDBFile> files = template.find(null);
-        assertThat(files).hasSize(1);
-        files.forEach(f -> {
-            try {
-                System.out.println(f.toString());
-                assertThat(ByteStreams.toByteArray(f.getInputStream())).isEqualTo(data.getBytes());
-                assertThat(f.getMD5()).isEqualToIgnoringCase(asset.checksum());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+            List<GridFSDBFile> files = template.find(null);
+            assertThat(files).hasSize(1);
+            files.forEach(f -> {
+                try {
+                    System.out.println(f.toString());
+                    assertThat(ByteStreams.toByteArray(f.getInputStream())).isEqualTo(data.getBytes());
+                    assertThat(f.getMD5()).isEqualToIgnoringCase(asset.checksum());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+        } finally {
+            FileUtils.deleteQuietly(input);
+        }
     }
 
 
     @Test
     public void testFindByMD5() throws IOException {
-        String data="sdfsakjdfhkhsdkfhsdkjhfksdhfk";
-        Asset asset=new Asset(new File("bla.txt"));
-        resource.save(asset);
+        String data="bla";
+        final File input = File.createTempFile("bla",".tmp");
+        PrintWriter pw = new PrintWriter(input);
+        pw.write(data);
+        pw.close();
+        try {
+            Asset asset=new Asset(input);
+            resource.save(asset);
 
-        GridFSDBFile found = template.findOne(query(where("md5").is(asset.checksum().toLowerCase())));
-        assertThat(found).isNotNull();
-        assertThat(new String(ByteStreams.toByteArray(found.getInputStream()))).isEqualTo(data);
+            GridFSDBFile found = template.findOne(query(where("md5").is(asset.checksum().toLowerCase())));
+            assertThat(found).isNotNull();
+            assertThat(new String(ByteStreams.toByteArray(found.getInputStream()))).isEqualTo(data);
+        } finally {
+            FileUtils.deleteQuietly(input);
+        }
     }
     @Before
     @After
