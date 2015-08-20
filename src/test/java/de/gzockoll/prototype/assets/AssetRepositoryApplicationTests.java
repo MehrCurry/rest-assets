618f7f63510c031bba8dc485bede89b34800c58f
Guido Zockoll <gzockoll@gmail.com>
2015-08-20 06:33:01 +0200

    test setup

--- src/test/java/de/gzockoll/prototype/assets/AssetRepositoryApplicationTests.java
+++ src/test/java/de/gzockoll/prototype/assets/AssetRepositoryApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AssetRepositoryApplication.class)
 @WebAppConfiguration
+@IntegrationTest("server.port:0")
+@ActiveProfiles("test")
 @Category(IntegrationTest.class)
 public class AssetRepositoryApplicationTests {
 
