68ffa6b072481a5cb1b1bd58251a400f1966908f
jkieras <jkieras@dd84334e-c5c9-4753-91c5-bdab5b6af16f>
2015-08-28 08:06:50 +0000

    Einbau von Swagger mittels SPringfox-Framework
    git-svn-id: svn://neptun.i.vjoon.com/SVN/java/blue/trunk/se.core@329906 dd84334e-c5c9-4753-91c5-bdab5b6af16f

--- src/main/java/com/vjoon/se/core/AssetRepositoryApplication.java
+++ src/main/java/com/vjoon/se/core/AssetRepositoryApplication.java
@@ -1,7 +1,8 @@
 package com.vjoon.se.core;
 
 import com.google.common.eventbus.AsyncEventBus;
-import com.google.common.eventbus.EventBus;
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.MultipartConfigFactory;
@@ -10,16 +11,37 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import javax.servlet.MultipartConfigElement;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import com.google.common.eventbus.EventBus;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@EnableSwagger2
 public class AssetRepositoryApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AssetRepositoryApplication.class, args);
+    }
+
+    @Bean
+    public Docket createDocket() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().paths(PathSelectors.any()).apis(
+                RequestHandlerSelectors.any()).build();
+    }
+    @Bean
+    public EventBus eventBus() {
+        Executor executor= Executors.newFixedThreadPool(10);
+        return new AsyncEventBus(executor);
     }
 
     @Bean
@@ -29,16 +51,16 @@ public class AssetRepositoryApplication {
 
     @Bean
     MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
+        final MultipartConfigFactory factory = new MultipartConfigFactory();
         factory.setMaxFileSize("5120MB");
         factory.setMaxRequestSize("5120MB");
         return factory.createMultipartConfig();
     }
 
-    @Bean
-    public EventBus eventBus() {
-        Executor executor= Executors.newFixedThreadPool(10);
-        return new AsyncEventBus(executor);
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("StorageEngine API").description("API to store and download assets")
+                .termsOfServiceUrl("Dead Link").contact("www.vjoon.com").license("Apache License Version 2.0").version(
+                        "2.0").build();
     }
 
 }
