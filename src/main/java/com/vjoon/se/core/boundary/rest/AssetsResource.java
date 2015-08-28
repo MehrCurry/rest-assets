9bc675e5361d62ae5e48563a7f6683093a8e570a
Guido Zockoll <gzockoll@gmail.com>
2015-08-28 07:38:29 +0200

    Swagger

--- src/main/java/com/vjoon/se/core/boundary/rest/AssetsResource.java
+++ src/main/java/com/vjoon/se/core/boundary/rest/AssetsResource.java
@@ -1,9 +1,9 @@
 package com.vjoon.se.core.boundary.rest;
 
-import com.vjoon.se.core.boundary.StreamHelper;
 import com.vjoon.se.core.control.MediaController;
 import com.vjoon.se.core.entity.Media;
 import com.vjoon.se.core.repository.MediaRepository;
+import com.wordnik.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -15,17 +15,14 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
-@RestController @Slf4j public class MediaResource {
+@Api(basePath = "/assets", value = "Assets", description = "Operations with Assets", produces = "application/json")
+@RestController @Slf4j @RequestMapping(value = "/assets")public class AssetsResource {
 
     @Autowired private MediaController controller;
 
     @Autowired private MediaRepository repository;
 
-    @Autowired private StreamHelper streamHelper;
-
-    @RequestMapping(value = "/assets", method = RequestMethod.POST) public @ResponseBody HttpEntity handleFileUpload(
+    @RequestMapping(method = RequestMethod.POST) public @ResponseBody HttpEntity handleFileUpload(
             @RequestParam(value = "file", required = true) MultipartFile file,
             @RequestParam(value = "key", required = true) String ref,
             @RequestParam(value = "namespace", required = true) String nameSpace) throws IOException {
@@ -34,19 +31,12 @@ import static com.google.common.base.Preconditions.checkArgument;
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-    @RequestMapping(value = "/assets", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public List<Media> findAll() {
         return repository.findAll();
     }
 
-    @RequestMapping(value = "/asset/{id}", method = RequestMethod.DELETE)
-    public HttpEntity deleteDocument(@PathVariable String id) throws IOException {
-        checkArgument(id != null);
-        controller.delete(id);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
-
-    @RequestMapping(value = "/assets", method = RequestMethod.DELETE) public HttpEntity deleteAll() throws IOException {
+    @RequestMapping(method = RequestMethod.DELETE) public HttpEntity deleteAll() throws IOException {
         controller.deleteAll();
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
