e418e8dc35f5688ac2fe0fba30401ad3bde9c0af
omeske <omeske@dd84334e-c5c9-4753-91c5-bdab5b6af16f>
2015-09-09 09:08:52 +0000

    StorageEngine eclipse settings angepasst.
    git-svn-id: svn://neptun.i.vjoon.com/SVN/java/blue/trunk/se.core@331208 dd84334e-c5c9-4753-91c5-bdab5b6af16f

--- src/main/java/com/vjoon/se/core/repository/AssetRepository.java
+++ src/main/java/com/vjoon/se/core/repository/AssetRepository.java
@@ -1,16 +1,17 @@
 package com.vjoon.se.core.repository;
 
-import com.vjoon.se.core.entity.Asset;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import com.vjoon.se.core.entity.Asset;
 
 public interface AssetRepository extends JpaRepository<Asset, Long> {
 
-    List<Asset> findByMediaId(String id);
-
     List<Asset> findByExistsInProduction(boolean b);
+
+    List<Asset> findByMediaId(String id);
 
     @Query("select a from Asset a where a.existsInProduction = false and a.snapshots is empty")
     List<Asset> findByNotExistsInProductionAndSnapshotsIsEmpty();
